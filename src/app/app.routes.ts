cc0308424008b2f75fa1c31698c81fde7216395b
Julio Magalhaes <julio.magalhaes@maps.com.br>
2024-05-29 09:44:42 -0300

    fix: small fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,5 @@
-import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'valorant', pathMatch: 'full' },
-  {
-    path: 'valorant',
-    loadChildren: () => loadRemoteModule('esr-valorant', './routes').then(m => m.routes)
-  },
-  {
-    path: 'league-of-legends',
-    loadChildren: () => loadRemoteModule('esr-valorant', './routes').then(m => m.routes)
-  },
 ];
