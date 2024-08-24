71636ce819a932b40aa18438cedc45b3d9c33a2e
Julio Magalhaes <julio.magalhaes@maps.com.br>
2024-08-24 17:49:18 -0300

    fix: small fixes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -10,13 +10,11 @@ export const DEV_URL = '';
 
 const REVERSE_PROXY_URL = `${PROTOCOL}//${HOSTNAME}:${PORT}`;
 const BACKEND_URL_CONST = `${REVERSE_PROXY_URL}`;
-const MFE_PROJECT_URL = (window as any)["env"]['MFE_PROJECT_URL'];
 
 export const environment = {
   production: false,
   REVERSE_PROXY: REVERSE_PROXY_URL,
   BACKEND_URL: BACKEND_URL_CONST,
-  MFE_PROJECT_URL: MFE_PROJECT_URL
 };
 
 /*
