cd8981468385cad75edf72251f802c80ccb8484d
Julio Magalhaes <julio.magalhaes@maps.com.br>
2024-08-24 17:36:59 -0300

    fix: small fixes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { initFederation } from '@angular-architects/native-federation';
+import { environment } from './environments/environment';
 
-initFederation('./assets/federation.manifest.json')
+initFederation(`./assets/federation.manifest${environment.production ? '.production' : ''}.json`)
   .catch(err => console.error(err))
   .then(res => {
     console.log(res);
