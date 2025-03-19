31faf0ebb6228e0aa86e99afba34943f3c94bde4
Julio Magalhaes <julio.magalhaes@maps.com.br>
2025-03-19 19:39:35 -0300

    fix: handle for prod environment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { loadRemoteModule } from '@angular-architects/native-federation';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'valorant', pathMatch: 'full' },
+  {
+    path: 'valorant',
+    loadChildren: () => loadRemoteModule('esr-valorant', './Valorant').then(m => m.routes),
+  },
+];
