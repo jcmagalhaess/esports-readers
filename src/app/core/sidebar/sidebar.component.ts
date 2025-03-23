a89591f317960619cac37253c780b48a15799a43
Julio Magalhaes <julio.magalhaes@maps.com.br>
2025-03-23 17:29:50 -0300

    feat: created leagues component

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { LeaguesComponent } from '../leagues/leagues.component';
 import { PlayNowComponent } from '../play-now/play-now.component';
 import { WeeklyRankingComponent } from '../weekly-ranking/weekly-ranking.component';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [PlayNowComponent, WeeklyRankingComponent],
+  imports: [PlayNowComponent, WeeklyRankingComponent, LeaguesComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
